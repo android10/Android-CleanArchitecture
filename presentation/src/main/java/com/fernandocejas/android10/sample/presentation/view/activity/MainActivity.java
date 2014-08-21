ba66c30a2566ff9f7f1462e33251620630ca7735
Fernando Cejas <fcejas@gmail.com>
2014-08-21 03:15:03 +0200

    Added test libraries for presentation layer and icon and image resources

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -2,13 +2,33 @@ package com.fernandocejas.android10.sample.presentation.view.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.fernandocejas.android10.sample.presentation.R;
 
 public class MainActivity extends Activity {
+
+  private Button btn_LoadData;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mapGUI();
   }
+
+  /**
+   * Maps the graphical user interface controls.
+   */
+  private void mapGUI() {
+    btn_LoadData = (Button)findViewById(R.id.btn_LoadData);
+    btn_LoadData.setOnClickListener(loadDataOnClickListener);
+  }
+
+  private final View.OnClickListener loadDataOnClickListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
 }
