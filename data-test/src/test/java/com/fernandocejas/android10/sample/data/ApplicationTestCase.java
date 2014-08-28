0dc6439b58912f56930f4521f888c4e0661b5579
Fernando Cejas <fcejas@gmail.com>
2014-08-29 01:52:10 +0200

    Added Application Test Case and Runner for Robolectric tests

---
+++ data-test/src/test/java/com/fernandocejas/android10/sample/data/ApplicationTestCase.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2014 android10.org. All rights reserved.
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.fernandocejas.android10.sample.data;
+
+import java.lang.reflect.Field;
+import org.junit.runner.RunWith;
+
+/**
+ * Base class for Robolectric data layer tests running with a custom Test Runner.
+ * Inherit from this class to create a test.
+ */
+@RunWith(ApplicationTestRunner.class)
+public class ApplicationTestCase {
+
+  /**
+   * Resets a Singleton class.
+   * This works using reflection and looking for a private field in the singleton called "INSTANCE".
+   * It is actually a workaround (hack?) to avoid global state when testing in isolation.
+   *
+   * @param clazz The class to reset.
+   */
+  protected void resetSingleton(Class clazz) {
+    Field instance;
+    try {
+      instance = clazz.getDeclaredField("INSTANCE");
+      instance.setAccessible(true);
+      instance.set(null, null);
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ data-test/src/test/java/com/fernandocejas/android10/sample/data/ApplicationTestRunner.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2014 android10.org. All rights reserved.
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.fernandocejas.android10.sample.data;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.AndroidManifest;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.Fs;
+
+/**
+ * Custom test runner for robolectric unit/integration tests.
+ */
+public class ApplicationTestRunner extends RobolectricTestRunner {
+
+  //Maximun SDK Robolectric will compile (issues with SDK > 18)
+  private static final int MAX_SDK_SUPPORTED_BY_ROBOLECTRIC = 18;
+
+  private static final String ANDROID_MANIFEST_PATH = "../data/src/main/AndroidManifest.xml";
+  private static final String ANDROID_MANIFEST_RES_PATH = "../data/src/main/res";
+
+  /**
+   * Call this constructor to specify the location of resources and AndroidManifest.xml.
+   *
+   * @param testClass
+   * @throws org.junit.runners.model.InitializationError
+   */
+  public ApplicationTestRunner(Class<?> testClass) throws InitializationError {
+    super(testClass);
+  }
+
+  @Override protected AndroidManifest getAppManifest(Config config) {
+    return new AndroidManifest(Fs.fileFromPath(ANDROID_MANIFEST_PATH),
+        Fs.fileFromPath(ANDROID_MANIFEST_RES_PATH)) {
+      @Override
+      public int getTargetSdkVersion() {
+        return MAX_SDK_SUPPORTED_BY_ROBOLECTRIC;
+      }
+    };
+  }
+}
