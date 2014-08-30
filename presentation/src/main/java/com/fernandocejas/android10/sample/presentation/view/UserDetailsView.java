31d0ca6cc6aced7a92e7ee45eaba44518c083fb2
Fernando Cejas <fcejas@gmail.com>
2014-08-30 21:58:24 +0200

    Added missing files and refactored views interfaces in presentation layer.

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/UserDetailsView.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/UserDetailsView.java
@@ -4,51 +4,17 @@
  */
 package com.fernandocejas.android10.sample.presentation.view;
 
-import android.content.Context;
 import com.fernandocejas.android10.sample.presentation.model.UserModel;
 
 /**
  * Interface representing a View in a model view presenter (MVP) pattern.
  * In this case is used as a view representing a user profile.
  */
-public interface UserDetailsView {
-
-  /**
-   * Show a view with a progress bar indicating a loading process.
-   */
-  void showLoading();
-
-  /**
-   * Hide a loading view.
-   */
-  void hideLoading();
-
-  /**
-   * Show a retry view in case of an error when retrieving data.
-   */
-  void showRetry();
-
-  /**
-   * Hide a retry view shown if there was an error when retrieving data.
-   */
-  void hideRetry();
-
+public interface UserDetailsView extends LoadDataView {
   /**
    * Render a user in the UI.
    *
    * @param user The {@link UserModel} that will be shown.
    */
   void renderUser(UserModel user);
-
-  /**
-   * Show an error message
-   *
-   * @param message A string representing an error.
-   */
-  void showError(String message);
-
-  /**
-   * Get a {@link android.content.Context}.
-   */
-  Context getContext();
 }
--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/UserListView.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/UserListView.java
@@ -4,7 +4,6 @@
  */
 package com.fernandocejas.android10.sample.presentation.view;
 
-import android.content.Context;
 import com.fernandocejas.android10.sample.presentation.model.UserModel;
 import java.util.Collection;
 
@@ -12,44 +11,11 @@ import java.util.Collection;
  * Interface representing a View in a model view presenter (MVP) pattern.
  * In this case is used as a view representing a list of {@link UserModel}.
  */
-public interface UserListView {
-
-  /**
-   * Show a view with a progress bar indicating a loading process.
-   */
-  void showLoading();
-
-  /**
-   * Hide a loading view.
-   */
-  void hideLoading();
-
-  /**
-   * Show a retry view in case of an error when retrieving data.
-   */
-  void showRetry();
-
-  /**
-   * Hide a retry view shown if there was an error when retrieving data.
-   */
-  void hideRetry();
-
+public interface UserListView extends LoadDataView {
   /**
    * Render a user list in the UI.
    *
    * @param userModelCollection The collection of {@link UserModel} that will be shown.
    */
   void renderUserList(Collection<UserModel> userModelCollection);
-
-  /**
-   * Show an error message
-   *
-   * @param message A string representing an error.
-   */
-  void showError(String message);
-
-  /**
-   * Get a {@link android.content.Context}.
-   */
-  Context getContext();
 }
