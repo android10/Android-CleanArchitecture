5195587dafc6f2b68cd2b6fd49873a63b2f73dbf
Fernando Cejas <fcejas@gmail.com>
2015-02-18 23:46:58 +0100

    Add android application class and presentation module.

---
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/AndroidApplication.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2015 android10.org. All rights reserved.
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.fernandocejas.android10.sample.presentation;
+
+import android.app.Application;
+
+/**
+ * Android Main Application
+ */
+public class AndroidApplication extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
---
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/PresentationModule.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2015 android10.org. All rights reserved.
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.fernandocejas.android10.sample.presentation;
+
+import dagger.Module;
+
+/**
+ * Dagger module that provides Presentation layer dependencies.
+ */
+@Module
+public class PresentationModule {
+}
