faa0d2a379e50cb60204cbb47ef72af0ac58f92e
Fernando Cejas <fcejas@gmail.com>
2015-04-10 22:08:11 +0200

    Fix mistyping.

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/internal/di/PerActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/internal/di/PerActivity.java
@@ -11,7 +11,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * A scoping annotation to permit objects whose lifetime should
- * conform to the life of the activity to be memoized in the
+ * conform to the life of the activity to be memorized in the
  * correct component.
  */
 @Scope
