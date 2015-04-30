7219fd0270754415e82d8dab2a982f67445825fb
Fernando Cejas <fcejas@gmail.com>
2015-04-30 22:34:47 +0200

    Update class headers with Apache License in data layer tests.

--- data-test/src/test/java/com/fernandocejas/android10/sample/data/ApplicationTestCase.java
+++ data-test/src/test/java/com/fernandocejas/android10/sample/data/ApplicationTestCase.java
@@ -1,6 +1,17 @@
 /**
- * Copyright (C) 2014 android10.org. All rights reserved.
- * @author Fernando Cejas (the android10 coder)
+ * Copyright (C) 2015 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.fernandocejas.android10.sample.data;
 
