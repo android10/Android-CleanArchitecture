18cfff71f9f738324e78227ce7dda23ec2812225
johannes orgis <hanneso@microsoft.com>
2016-01-19 12:36:42 +0100

    add leakcanary to detect and show the leak of the presenter (and everything it has references to) steps to reproduce: 1. deploy and start on device/emulator with no data network but with connection (so the REST connection times out) 2. put application to background while the  REST call is waiting for the server response 3. click on  leakcanary notification

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/AndroidApplication.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/AndroidApplication.java
@@ -16,29 +16,32 @@
 package com.fernandocejas.android10.sample.presentation;
 
 import android.app.Application;
+
 import com.fernandocejas.android10.sample.presentation.internal.di.components.ApplicationComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.components.DaggerApplicationComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.modules.ApplicationModule;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Android Main Application
  */
 public class AndroidApplication extends Application {
 
-  private ApplicationComponent applicationComponent;
+    private ApplicationComponent applicationComponent;
 
-  @Override public void onCreate() {
-    super.onCreate();
-    this.initializeInjector();
-  }
+    @Override public void onCreate() {
+        super.onCreate();
+        this.initializeInjector();
+        LeakCanary.install(this);
+    }
 
-  private void initializeInjector() {
-    this.applicationComponent = DaggerApplicationComponent.builder()
-        .applicationModule(new ApplicationModule(this))
-        .build();
-  }
+    private void initializeInjector() {
+        this.applicationComponent = DaggerApplicationComponent.builder()
+                                                              .applicationModule(new ApplicationModule(this))
+                                                              .build();
+    }
 
-  public ApplicationComponent getApplicationComponent() {
-    return this.applicationComponent;
-  }
+    public ApplicationComponent getApplicationComponent() {
+        return this.applicationComponent;
+    }
 }
