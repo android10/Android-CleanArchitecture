e0377a2a6195f015b53f791e114b5b9efec3409f
johannes orgis <hanneso@microsoft.com>
2016-01-19 12:39:35 +0100

    wrap the subscriptions with the decorator before subscribing (sorry for the formatter changes)

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/presenter/UserDetailsPresenter.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/presenter/UserDetailsPresenter.java
@@ -15,20 +15,23 @@
  */
 package com.fernandocejas.android10.sample.presentation.presenter;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import android.support.annotation.NonNull;
+
 import com.fernandocejas.android10.sample.domain.User;
 import com.fernandocejas.android10.sample.domain.exception.DefaultErrorBundle;
 import com.fernandocejas.android10.sample.domain.exception.ErrorBundle;
 import com.fernandocejas.android10.sample.domain.interactor.DefaultSubscriber;
 import com.fernandocejas.android10.sample.domain.interactor.UseCase;
+import com.fernandocejas.android10.sample.presentation.SubscriptionDecorator;
 import com.fernandocejas.android10.sample.presentation.exception.ErrorMessageFactory;
 import com.fernandocejas.android10.sample.presentation.internal.di.PerActivity;
 import com.fernandocejas.android10.sample.presentation.mapper.UserModelDataMapper;
 import com.fernandocejas.android10.sample.presentation.model.UserModel;
 import com.fernandocejas.android10.sample.presentation.view.UserDetailsView;
 import com.fernandocejas.frodo.annotation.RxLogSubscriber;
-import javax.inject.Inject;
-import javax.inject.Named;
 
 /**
  * {@link Presenter} that controls communication between views and models of the presentation
@@ -37,96 +40,98 @@ import javax.inject.Named;
 @PerActivity
 public class UserDetailsPresenter implements Presenter {
 
-  /** id used to retrieve user details */
-  private int userId;
+    /** id used to retrieve user details */
+    private int userId;
 
-  private UserDetailsView viewDetailsView;
+    private UserDetailsView viewDetailsView;
 
-  private final UseCase getUserDetailsUseCase;
-  private final UserModelDataMapper userModelDataMapper;
+    private final UseCase getUserDetailsUseCase;
+    private final UserModelDataMapper userModelDataMapper;
 
-  @Inject
-  public UserDetailsPresenter(@Named("userDetails") UseCase getUserDetailsUseCase,
-      UserModelDataMapper userModelDataMapper) {
-    this.getUserDetailsUseCase = getUserDetailsUseCase;
-    this.userModelDataMapper = userModelDataMapper;
-  }
-
-  public void setView(@NonNull UserDetailsView view) {
-    this.viewDetailsView = view;
-  }
-
-  @Override public void resume() {}
-
-  @Override public void pause() {}
-
-  @Override public void destroy() {
-    this.getUserDetailsUseCase.unsubscribe();
-  }
-
-  /**
-   * Initializes the presenter by start retrieving user details.
-   */
-  public void initialize(int userId) {
-    this.userId = userId;
-    this.loadUserDetails();
-  }
-
-  /**
-   * Loads user details.
-   */
-  private void loadUserDetails() {
-    this.hideViewRetry();
-    this.showViewLoading();
-    this.getUserDetails();
-  }
-
-  private void showViewLoading() {
-    this.viewDetailsView.showLoading();
-  }
-
-  private void hideViewLoading() {
-    this.viewDetailsView.hideLoading();
-  }
-
-  private void showViewRetry() {
-    this.viewDetailsView.showRetry();
-  }
-
-  private void hideViewRetry() {
-    this.viewDetailsView.hideRetry();
-  }
-
-  private void showErrorMessage(ErrorBundle errorBundle) {
-    String errorMessage = ErrorMessageFactory.create(this.viewDetailsView.getContext(),
-        errorBundle.getException());
-    this.viewDetailsView.showError(errorMessage);
-  }
-
-  private void showUserDetailsInView(User user) {
-    final UserModel userModel = this.userModelDataMapper.transform(user);
-    this.viewDetailsView.renderUser(userModel);
-  }
-
-  private void getUserDetails() {
-    this.getUserDetailsUseCase.execute(new UserDetailsSubscriber());
-  }
-
-  @RxLogSubscriber
-  private final class UserDetailsSubscriber extends DefaultSubscriber<User> {
-
-    @Override public void onCompleted() {
-      UserDetailsPresenter.this.hideViewLoading();
+    @Inject
+    public UserDetailsPresenter(@Named("userDetails") UseCase getUserDetailsUseCase,
+                                UserModelDataMapper userModelDataMapper) {
+        this.getUserDetailsUseCase = getUserDetailsUseCase;
+        this.userModelDataMapper = userModelDataMapper;
     }
 
-    @Override public void onError(Throwable e) {
-      UserDetailsPresenter.this.hideViewLoading();
-      UserDetailsPresenter.this.showErrorMessage(new DefaultErrorBundle((Exception) e));
-      UserDetailsPresenter.this.showViewRetry();
+    public void setView(@NonNull UserDetailsView view) {
+        this.viewDetailsView = view;
     }
 
-    @Override public void onNext(User user) {
-      UserDetailsPresenter.this.showUserDetailsInView(user);
+    @Override public void resume() {
     }
-  }
+
+    @Override public void pause() {
+    }
+
+    @Override public void destroy() {
+        this.getUserDetailsUseCase.unsubscribe();
+    }
+
+    /**
+     * Initializes the presenter by start retrieving user details.
+     */
+    public void initialize(int userId) {
+        this.userId = userId;
+        this.loadUserDetails();
+    }
+
+    /**
+     * Loads user details.
+     */
+    private void loadUserDetails() {
+        this.hideViewRetry();
+        this.showViewLoading();
+        this.getUserDetails();
+    }
+
+    private void showViewLoading() {
+        this.viewDetailsView.showLoading();
+    }
+
+    private void hideViewLoading() {
+        this.viewDetailsView.hideLoading();
+    }
+
+    private void showViewRetry() {
+        this.viewDetailsView.showRetry();
+    }
+
+    private void hideViewRetry() {
+        this.viewDetailsView.hideRetry();
+    }
+
+    private void showErrorMessage(ErrorBundle errorBundle) {
+        String errorMessage = ErrorMessageFactory.create(this.viewDetailsView.getContext(),
+                errorBundle.getException());
+        this.viewDetailsView.showError(errorMessage);
+    }
+
+    private void showUserDetailsInView(User user) {
+        final UserModel userModel = this.userModelDataMapper.transform(user);
+        this.viewDetailsView.renderUser(userModel);
+    }
+
+    private void getUserDetails() {
+        this.getUserDetailsUseCase.execute(new SubscriptionDecorator<>(new UserDetailsSubscriber()));
+    }
+
+    @RxLogSubscriber
+    private final class UserDetailsSubscriber extends DefaultSubscriber<User> {
+
+        @Override public void onCompleted() {
+            UserDetailsPresenter.this.hideViewLoading();
+        }
+
+        @Override public void onError(Throwable e) {
+            UserDetailsPresenter.this.hideViewLoading();
+            UserDetailsPresenter.this.showErrorMessage(new DefaultErrorBundle((Exception) e));
+            UserDetailsPresenter.this.showViewRetry();
+        }
+
+        @Override public void onNext(User user) {
+            UserDetailsPresenter.this.showUserDetailsInView(user);
+        }
+    }
 }
