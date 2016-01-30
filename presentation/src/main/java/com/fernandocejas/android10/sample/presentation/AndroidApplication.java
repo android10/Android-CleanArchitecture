b03050783aa5f956fb922129db85b451c52bde3d
Fernando Cejas <fcejas@gmail.com>
2016-01-30 15:54:30 +0100

    Add Leak canary to the project.

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/AndroidApplication.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/AndroidApplication.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +19,7 @@ import android.app.Application;
 import com.fernandocejas.android10.sample.presentation.internal.di.components.ApplicationComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.components.DaggerApplicationComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.modules.ApplicationModule;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Android Main Application
@@ -30,6 +31,7 @@ public class AndroidApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
     this.initializeInjector();
+    this.initializeLeakDetection();
   }
 
   private void initializeInjector() {
@@ -40,5 +42,11 @@ public class AndroidApplication extends Application {
 
   public ApplicationComponent getApplicationComponent() {
     return this.applicationComponent;
+  }
+
+  private void initializeLeakDetection() {
+    if (BuildConfig.DEBUG) {
+      LeakCanary.install(this);
+    }
   }
 }
